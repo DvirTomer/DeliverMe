26c697a155f888c53356af6763e9dca2a480fe1b
Mayplex <mayplex123@gmail.com>
2021-01-05 15:28:30 +0200

    add time and date. add rate

--- app/src/main/java/com/example/deliverme/Register.java
+++ app/src/main/java/com/example/deliverme/Register.java
@@ -89,7 +89,7 @@ public class Register extends AppCompatActivity {
                             String id =mhuth.getCurrentUser().getUid();
 //                            }
 //                            String id=dbUser.push().getKey();
-                            User user=new User(id,allName,email,phone,"0");
+                            User user=new User(id,allName,email,phone,"0",0.0);
                             dbUser.child(id).setValue(user);
                             dbUser= FirebaseDatabase.getInstance().getReference("users").child(id).child("packages");
                             Toast.makeText(Register.this, "User created",Toast.LENGTH_SHORT).show();
